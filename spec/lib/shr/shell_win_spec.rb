8314217a67860e346b4e5bfb2bc6f33299d8f97a
kukenko <m.kukenko@gmail.com>
2013-01-12 19:09:07 +0900

    Add shell_win_spec.rb
    Take out the cases depend on OS commands from shell_spec.rb

--- spec/lib/shr/shell_win_spec.rb
+++ spec/lib/shr/shell_win_spec.rb
@@ -5,22 +5,10 @@ require 'tempfile'
 require 'tmpdir'
 
 module Shr
-  describe Shell do
-    let(:sh) { Shell.new }
+  if OS.windows?
+    describe Shell do
+      let(:sh) { Shell.new }
 
-    it 'has the following methods' do
-      m = sh.methods
-      m.should include(:capture)
-      m.should include(:release)
-      m.should include(:exitstatus)
-      m.should include(:each)
-      m.should include(:redirect_from)
-      m.should include(:<)
-      m.should include(:redirect_to)
-      m.should include(:>)
-    end
-
-    unless OS.windows?
       before(:all) do
         @tmpdir = Dir.mktmpdir
         ['perl.pl', 'python.py', 'ruby.rb'].each do |file|
@@ -30,31 +18,23 @@ module Shr
       end
 
       it 'executes OS commands' do
-        cmd_result = `pwd`
-        sh_result  = sh.pwd.to_s
+        cmd_result = `chdir`
+        sh_result  = sh.chdir.to_s
         sh_result.should eq(cmd_result)
       end
 
       context "command end with '!'" do
         it 'executes OS commands' do
-          sh.touch! @tmpfile
+          sh.copy!('nul', @tmpfile.gsub('/', '\\'))
           File.should exist(@tmpfile)
           FileUtils.rm @tmpfile
         end
       end
 
-      describe '#capture' do
-        it 'captures output of commands'
-      end
-
-      describe '#release' do
-        it 'releases output of commands'
-      end
-
       describe '#each' do
         it 'yields each line (result of command) to the block' do
           files = []
-          sh.ls(@tmpdir).sort(:r).each do |file|
+          sh.dir(:B, @tmpdir.gsub('/', '\\')).sort(:R).each do |file|
             files << file.strip
           end
           files.should eq(['ruby.rb', 'python.py', 'perl.pl'])
@@ -62,38 +42,30 @@ module Shr
 
         context 'without block' do
           it 'returns Enumerator' do
-            sh.ls.each.should be_kind_of(Enumerator)
+            sh.dir.each.should be_kind_of(Enumerator)
           end
         end
       end
 
       it "emulates the shell pipe operator by method chaining" do
-        cmd_result = `pwd | tr '[:lower:]' '[:upper:]'`
-        sh_result = sh.pwd.tr("'[:lower:]'", "'[:upper:]'").to_s
+        cmd_result = `chdir | ruby -ne 'puts $_.tr("a-z", "A-Z")'`
+        sh_result = sh.chdir.ruby('-ne', %q{'puts $_.tr("a-z", "A-Z")'}).to_s
         sh_result.should eq(cmd_result)
-      end
-
-      describe '#exitstatus' do
-        it "shows an exit status of process" do
-          sh.ruby('-e', "'exit 0'").exitstatus.should eq(0)
-          sh.ruby('-e', "'exit 1'").exitstatus.should eq(1)
-        end
       end
 
       describe '#redirect_from' do
         it 'redirect from file to command' do
           tempfile = Tempfile.new('temp')
-          sh.pwd.redirect_to(tempfile.path)
-          sh.cat.redirect_from(tempfile.path).to_s.should eq(`pwd`)
+          sh.chdir.redirect_to(tempfile.path)
+          sh.more.redirect_from(tempfile.path).to_s.should eq(`chdir`)
           tempfile.close!
         end
       end
 
       describe '#redirect_to' do
         it 'redirecs result of command to file' do
-          tempfile = Tempfile.new('temp')
-          sh.pwd.redirect_to(tempfile.path)
-          File.read(tempfile.path).should eq(`pwd`)
+          sh.chdir.redirect_to(tempfile.path)
+          File.read(tempfile.path).should eq(`chdir`)
           tempfile.close!
         end
       end
