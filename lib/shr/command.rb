e7a3047b570eaabaff8dec7b084c8f65f3684216
kukenko <m.kukenko@gmail.com>
2013-01-13 22:40:57 +0900

    Change Shr::Command (in progress)
    for refactoring
    Shr::WhichをShr::Commandの中で呼び出すように変更する予定

--- lib/shr/command.rb
+++ lib/shr/command.rb
@@ -4,21 +4,21 @@ require 'shr/option'
 
 module Shr
   class Command
-    # xxx
     def initialize(command, options)
+      @command = command
+      # xxx
       option = Option.new
       option.indicator = '/' if OS.windows?
-      opts = option.parse(options).join(' ')
+      @opts = option.parse(options).join(' ')
+    end
+    attr_reader :command
 
-      @command = if OS.windows?
-                   "cmd /c #{command} #{opts}".strip
-                 else
-                   "#{command} #{opts}".strip
-                 end
+    def command
+      OS.windows? ? "cmd /c #{@command}" : @command
     end
 
     def to_s
-      @command
+      "#{@command} #{@opts}".strip
     end
   end
 end
